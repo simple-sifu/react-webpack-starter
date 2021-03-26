94402f9e9ac136d3c177bf719f948ab22ac10473
Tommy Han <tommy_han1@intuit.com>
2021-03-26 01:17:23 -0400

    add sass file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ require('file-loader?name=[name].[ext]!./index.html')
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import './App.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
